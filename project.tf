be758566ee133d5349c4f9e72c9c6676379372aa
Guillaume Rouillé <rouille.guillaume@gmail.com>
2025-01-30 16:21:33 +0100

    add ssh key

--- project.tf
+++ project.tf
@@ -98,3 +98,21 @@ resource "aws_db_subnet_group" "default" {
     Name = "${var.identifiant}_SUBNET_GROUP_RDS"
   }
 }
+
+resource "aws_security_group_rule" "ec2_to_http" {
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.ec2.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "ec2_to_https" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.ec2.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
